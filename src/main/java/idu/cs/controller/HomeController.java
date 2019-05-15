3a8f85d91ed2fef5a1d6be1f7eb775394f354b12
egyou <egyou@induk.ac.kr>
2019-05-15 17:13:34 +0900

    commit20190515

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -5,21 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import idu.cs.domain.User;
+import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
 public class HomeController {
 	@Autowired UserRepository userRepo; // Dependency Injection
-	
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("test", "인덕 컴소");
 		model.addAttribute("egy", "유응구");
 		return "index";
+	}
+	@GetMapping("/register")
+	public String getRegForm(Model model) {
+		return "form";
 	}
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
@@ -27,9 +32,18 @@ public class HomeController {
 		return "userlist";
 	}
 	@PostMapping("/users")
-	public String createUser(@Valid @RequestBody User user, Model model) {
+	public String createUser(@Valid @RequestBody User user, Model model) {		
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
+	}
+	@GetMapping("/users/{id}")
+	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId).get();//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		model.addAttribute("name", user.getName());
+		model.addAttribute("company", user.getCompany());
+		return "user";
+		//return ResponseEntity.ok().body(user);
 	}
 }
